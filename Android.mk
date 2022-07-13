e54aef0b05158754dd1e934a0fe3f98d35f96e56
krasCGQ <kras@raphielgang.org>
2022-07-13 21:06:01 +0100

    wayne: Override several props to allow stock ROM flashing
    * Stock firmware zip checks for ro.build.date.utc, which's false-positive
      since TWRP's build date might be newer than stock firmware that we're
      going to flash.
    * Also, just for kicks and giggles, override ro.bootimage.build.date.utc
      so we can have matching yet consistent POSIX build time.

--- Android.mk
+++ Android.mk
@@ -29,6 +29,11 @@ ifeq ($(TARGET_DEVICE),wayne)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+# Hack some props to allow stock ROM flashing
+BOARD_RECOVERY_IMAGE_PREPARE := \
+    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default
+
 include $(CLEAR_VARS)
 
 endif
