1b70e3e73e17477cc7943df3e22f2d0758c8cbb9
xiaoleGun <1592501605@qq.com>
2022-11-26 19:46:53 +0800

    wayne: Retrofit to dynamic partition
    * Sync ROM device tree
    - Retrofit rawdump as metadata
    - Specify androidboot.boot_devices
    - Sync boot image header with stock
    - Build fastbootd
    - Change partition tables for dynamic partition
    - Update prebuit kernel to v4.19.265 for dynamic partition
    
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -1,3 +1,26 @@
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.boot.dynamic_partitions=true \
+	ro.boot.dynamic_partitions_retrofit=true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.fastbootd.available=true
+
 # Qcom decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
